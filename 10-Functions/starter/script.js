b237954d2274d834407789f29cf1186b386e0922
Tiago Machado <tmxx37@gmail.com>
2022-04-11 11:15:48 +0100

    functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -289,8 +289,84 @@ runOnce();
 })(); //IIFE - immediatly invoked function expression (só é invocada uma vez)
 
 (() => console.log('This will ALSO never run again'))();
-*/
+
 
 const secureBooking = function () {
   let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
 };
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+
+//Example1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+//Re-assigning f function
+h();
+f();
+
+//Example2 -> closure tem prioridade sobre variaveis no global scope
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3; //3 é o numero de grupos que existem normalmente
+
+  setTimeout(function () {
+    console.log(`We are boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
+
+////////////////////
+// Coding Challenge #2
+// This is more of a thinking challenge than a coding challenge �
+// Your tasks:
+// 1. Take the IIFE below and at the end of the function, attach an event listener that
+// changes the color of the selected h1 element ('header') to blue, each time
+// the body element is clicked. Do not select the h1 element again!
+// 2. And now explain to yourself (or someone around you) why this worked! Take all
+// the time you need. Think about when exactly the callback function is executed,
+// and what that means for the variables involved in this example.
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+    setTimeout(function () {
+      header.style.color = 'red';
+    }, 1500);
+  });
+})();
